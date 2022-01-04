4718cf6b19cbec683c87305fce19b1d2dc5e3973
Citizen-Oleg <oleg-12-00@bk.ru>
2022-01-04 21:54:43 +0300

    Game acceleration

---
+++ Assets/Scripts/View/GameSpeedView.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+using UnityEngine;
+
+namespace View
+{
+    public class GameSpeedView : MonoBehaviour
+    {
+        [UsedImplicitly]
+        public void SetSpeedGame(int speed)
+        {
+            Time.timeScale = speed;
+        }
+    }
+}
