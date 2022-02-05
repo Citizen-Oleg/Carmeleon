caaa7c0096671db12af8fb4a490fa02750318ab0
Citizen-Oleg <oleg-12-00@bk.ru>
2022-02-05 20:56:14 +0300

    Small folder transfer. New buff to increase certain resistances.

---
+++ Assets/Scripts/ScriptsLevels/BuffSystem/Buffs/PassiveDamageTypeResistanceIncrease.cs
@@ -0,0 +1,56 @@
+﻿using EnemyComponent;
+using Interface;
+using Towers;
+
+namespace BuffSystem.Buffs
+{
+    public class PassiveDamageTypeResistanceIncrease : IPassiveBuff
+    {
+        private readonly Enemy _enemy;
+        private readonly DamageType _damageType;
+        private readonly int _percentageReduction;
+        
+        public PassiveDamageTypeResistanceIncrease(Enemy enemy, DamageType damageType, int percentageReduction)
+        {
+            _enemy = enemy;
+            _damageType = damageType;
+            _percentageReduction = percentageReduction;
+        }
+
+        public bool IsActive { get; private set; }
+        
+        public void Start()
+        {
+            IsActive = true;
+            SetPercentageReduction();
+        }
+
+        public void Stop()
+        {
+            IsActive = false;
+            SetPercentageReduction();
+        }
+
+        private void SetPercentageReduction()
+        {
+            switch (_damageType)
+            {
+                case DamageType.Physical:
+                    _enemy.CharacteristicsEnemy.Armor += IsActive ? _percentageReduction : -_percentageReduction;
+                     break;
+                case DamageType.Fire:
+                    _enemy.CharacteristicsEnemy.FireResistance += IsActive ? _percentageReduction : -_percentageReduction;
+                     break;
+                case DamageType.Air:
+                    _enemy.CharacteristicsEnemy.AirResistance += IsActive ? _percentageReduction : -_percentageReduction;
+                     break;
+                case DamageType.Earth:
+                    _enemy.CharacteristicsEnemy.EarthResistance += IsActive ? _percentageReduction : -_percentageReduction;
+                     break;
+                case DamageType.Water:
+                    _enemy.CharacteristicsEnemy.WaterResistance += IsActive ? _percentageReduction : -_percentageReduction;
+                     break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ScriptsLevels/BuffSystem/SettingsBuff/EnemySettings/SettingsPassiveDamageTypeResistanceIncreaseEnemy.cs
@@ -0,0 +1,23 @@
+﻿using BuffSystem.Buffs;
+using EnemyComponent;
+using Interface;
+using Towers;
+using UnityEngine;
+
+namespace BuffSystem.SettingsBuff.EnemySettings
+{
+    [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/BuffSettings/EnemyBuff/IncreaseResistanceDamageType", order = 14)]
+    public class SettingsPassiveDamageTypeResistanceIncreaseEnemy : SettingsBuff<Enemy>
+    {
+        [SerializeField]
+        private DamageType damageType;
+        [Range(0, 100)]
+        [SerializeField]
+        private int percentageReduction;
+        
+        public override IPassiveBuff GetBuff(Enemy buffObject)
+        {
+            return new PassiveDamageTypeResistanceIncrease(buffObject, damageType, percentageReduction);
+        }
+    }
+}
