f7e473e7ab1f2ffa3ab6497ddc1bf292263c7846
Citizen-Oleg <oleg-12-00@bk.ru>
2022-02-13 17:28:48 +0300

    Workshop setup

--- Assets/Scripts/ScriptsMenu/Tree/TalentScriptableObject/InventorySlotUnlockTalent.cs
+++ Assets/Scripts/ScriptsMenu/Tree/TalentScriptableObject/InventorySlotUnlockTalent.cs
@@ -5,14 +5,17 @@ namespace ScriptsMenu.Tree.TalentScriptableObject
     [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Talent/InventorySlotUnlock", order = 4)]
     public class InventorySlotUnlockTalent : Talent
     {
+        [SerializeField]
+        private int _count;
+        
         public override void ActivateTalent()
         {
-            GameManager.PlayerData.InventorySize++;
+            GameManager.PlayerData.InventorySize += _count;
         }
 
         public override void DeactivateTalent()
         {
-            GameManager.PlayerData.InventorySize--;
+            GameManager.PlayerData.InventorySize -= _count;
         }
     }
 }
