6347f4bbb582e701b5a92fa6832d489138a70e26
Citizen-Oleg <oleg-12-00@bk.ru>
2022-02-18 11:56:25 +0300

    Fake buff for a bestiary. New view with explanations for modifiers.

---
+++ Assets/Scripts/ScriptsLevels/BuffSystem/SettingsBuff/EnemySettings/FakeBuff.cs
@@ -0,0 +1,16 @@
+﻿using BuffSystem.SettingsBuff;
+using EnemyComponent;
+using Interface;
+using UnityEngine;
+
+namespace ScriptsLevels.BuffSystem
+{
+    [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/BuffSettings/EnemyBuff/Fake", order = 20)]
+    public class FakeBuff : SettingsBuff<Enemy>
+    {
+        public override IPassiveBuff GetBuff(Enemy buffObject)
+        {
+            return null;
+        }
+    }
+}
